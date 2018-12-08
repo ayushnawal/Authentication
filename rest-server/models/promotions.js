071cc273773cc51b3bcb3dc976fe15d6afe97ca0
Ayush Nawal <36100944+ayushnawal@users.noreply.github.com>
2018-12-09 04:33:23 +0530

    remove debug.log

---
+++ rest-server/models/promotions.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var promotionSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    default: ''
+  },
+  price: {
+    type: Currency,
+    required: true,
+    min: 0
+  },
+  description: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true,
+  collection: 'promotions'
+});
+
+var Promotions = mongoose.model('Promotion', promotionSchema);
+
+module.exports = Promotions;
